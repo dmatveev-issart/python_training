4c47e444bf6c9efc6bd44274f3ea4cb070fd0361
dmatveev <dmatveev@issart.com>
2019-08-25 11:18:16 +0600

    3 login and logout moved to fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,6 +3,4 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="denis", middlename="sergeevich", lastname="matveev", nickname="dmatveev", company="issart", address="omsk", email="dmatveev@issart.com"))
-    app.session.logout()
