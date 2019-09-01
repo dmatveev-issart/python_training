ba818da723438dc32d5a1c000cf90ff9778f3be0
dmatveev <dmatveev@issart.com>
2019-09-01 18:22:13 +0600

    4 another assertion was added to test_del_group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -14,4 +14,7 @@ def test_modify_group_name(app):
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Test group", header="Test header", footer="Test footer"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
