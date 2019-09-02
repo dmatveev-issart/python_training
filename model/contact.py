2d1c1d2121d8ddc5dd120675e1848e16b7f12ff2
dmatveev <dmatveev@issart.com>
2019-09-02 22:25:07 +0600

    4 11.2 completed

--- model/contact.py
+++ model/contact.py
@@ -13,3 +13,9 @@ class Contact:
         self.email = email
         self.id = id
         self.name = name
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
