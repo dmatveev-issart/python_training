d20dae653e4fc69ce46f9a48e732b5baa078019d
dmatveev <dmatveev@issart.com>
2019-09-08 14:41:20 +0600

    5 before doing of 14 task

--- fixture/contact.py
+++ fixture/contact.py
@@ -97,8 +97,8 @@ class ContactHelper:
             self.contact_cache = []
             for row in wd.find_elements_by_name("entry"):
                 cells = row.find_elements_by_tag_name("td")
-                firstname = cells[1].text
-                lastname = cells[2].text
+                firstname = cells[2].text
+                lastname = cells[1].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 all_phones = cells[5].text
                 self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
