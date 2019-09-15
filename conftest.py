5fc4eae9fcd8ed82b901f1fd8914fecd1488b50d
dmatveev <dmatveev@issart.com>
2019-09-15 16:07:48 +0600

    6 17 task is complete

--- conftest.py
+++ conftest.py
@@ -2,9 +2,11 @@
 import json
 from fixture.application import Application
 import pytest
+import os.path
 
 fixture = None
 target = None
+
 
 @pytest.fixture
 def app(request):
@@ -12,8 +14,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target['baseUrl'])
     fixture.session.ensure_login(username=target['username'], password=target['password'])
