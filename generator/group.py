e6f66ed4c5c477d9c228d0705a1e00d52fadef46
dmatveev <dmatveev@issart.com>
2019-09-21 19:23:38 +0600

    7 4 lection complete

--- generator/group.py
+++ generator/group.py
@@ -30,10 +30,16 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [Group(name="", header="", footer="")] + [
+# without empty group:
+testdata = [Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(n)]
+
+
+    # with empty group:
+"""testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
     for i in range(n)
-]
+]"""
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
