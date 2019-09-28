e4efb717f8d84e186e772a9e4503d4aa49dcd879
dmatveev <dmatveev@issart.com>
2019-09-28 13:52:00 +0600

    7 1 task groups add del mod are completed

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_modify_group_name(app, json_groups):
-    group = json_groups
-    if app.group.count() == 0:
+def test_modify_group_name(app, json_groups, db, check_ui):
+    group_data = json_groups
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name="Test group", header="Test header", footer="Test footer"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group.id = old_groups[index].id
-    app.group.modify_group_by_index(index, group)
-    assert len(old_groups) == (app.group.count())
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
+
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    index = old_groups.index(group)
+
+    app.group.modify_group_by_id(group.id, group_data)
+    #assert len(old_groups) == (app.group.count())
+    new_groups = db.get_group_list()
+    old_groups[index] = new_groups[index]
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+
 
